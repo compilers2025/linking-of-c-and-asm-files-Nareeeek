ac7de5d844bfc005f02929d6b00509388e48c779
Narek <narek_lazyan@edu.aua.am>
2025-02-07 15:53:34 +0400

    math returns 42, the answer to the universe I'm guessing? Also might have commited way too many files, I forgot which we are supposed to commit and what not to, if any are missing please also tell me

--- math.s
+++ math.s
@@ -5,14 +5,9 @@
 	.type	math_func, @function
 math_func:
 
-   # this moves variable i from the memory to the %rax register
-   mov i, %rax 
-   # write something to move variable j to the %rbx register
-   ??????????
-   # do j - i
-   sub  ?, ? 
-   # at the end of the function %rax should contain the return of the function
-   # do something so for that:
-   ??????????
-   #function exits
-   ret
+    mov i(%rip), %rax 
+    mov j(%rip), %rbx 
+    sub %rax, %rbx 
+    mov %rbx, %rax  
+    ret
+   
